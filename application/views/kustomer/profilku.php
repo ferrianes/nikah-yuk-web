a54fb388169cdf2fb00d54caa39fd6bd8fe4f917
Ferrian Septiawan <ferrian27@gmail.com>
2020-10-06 14:08:57 +0700

    edit profilku

--- application/views/kustomer/profilku.php
+++ application/views/kustomer/profilku.php
@@ -1,14 +1,15 @@
-<div class="container mt-5">
-	<?= $this->session->flashdata('pesan'); ?>
-	<!-- Data Kustomer -->
-	<div class="row">
-		<div class="col">
-			<div class="card shadow">
-				<div class="card-body">
-					<h5 class="card-title">Profilku</h5>
+<section class="section bg-white">
+	<div class="container-fluid">
+		<div class="card card-product shadow mt--540">
+			<div class="p-4">
+				<div class="container">
+					<?= $this->session->flashdata('pesan'); ?>
+					<h2 class="text-center mb-4">Profilku</h2>
 					<div class="row">
 						<div class="col-12 col-md-3 text-center">
-							<img src="<?= base_url('assets/img/api/kustomer/' . $kustomer['image']); ?>" alt="Raised circle image" class="rounded-circle shadow-lg" style="width: 200px; height: 200px; object-fit:cover;">
+							<img src="<?= base_url('assets/img/api/kustomer/' . $kustomer['image']); ?>"
+								alt="Raised circle image" class="rounded-circle shadow-lg"
+								style="width: 200px; height: 200px; object-fit:cover;">
 						</div>
 						<div class="col">
 							<h6 class="card-subtitle text-muted">Nama</h6>
@@ -23,11 +24,12 @@
 					</div>
 					<div class="row">
 						<div class="col-md-3 mt-4 text-center">
-							<a href="<?= base_url('kustomer/ubahprofil') ?>" class="btn btn-primary"><i class="fas fa-fw fa-edit mr-2"></i>Ubah Profil</a>
+							<a href="<?= base_url('kustomer/ubahprofil') ?>" class="btn btn-primary"><i
+									class="fas fa-fw fa-edit mr-2"></i>Ubah Profil</a>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</section>
