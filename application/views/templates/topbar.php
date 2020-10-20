d3ca87746b2fe932671bcf79184c94af6312af1e
Ferrian Septiawan <ferrian27@gmail.com>
2020-10-20 14:36:35 +0700

    change dir topbar admin

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -22,7 +22,7 @@
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span class="mr-2 d-none d-lg-inline text-gray-600 small">Hello, <?= $admin['username'] ?></span>
-                    <img class="img-profile rounded-circle" src="<?= base_url('assets/img/admin/' . $admin['image']); ?>">
+                    <img class="img-profile rounded-circle" src="<?= base_url('assets/img/api/admin/' . $admin['image']); ?>">
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
